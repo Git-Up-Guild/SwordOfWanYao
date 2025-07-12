cc79331292359acf8749e2a8091139dcdc84dd2e
mcaswen <mcaswen@gmail.com>
2025-07-13 01:21:41 +0800

    feat: implemented facing direction changes during soldier movement.

--- Assets/Game/ImmortalRecord/GameScripts/Soldier/SoldierMovement.cs
+++ Assets/Game/ImmortalRecord/GameScripts/Soldier/SoldierMovement.cs
@@ -6,13 +6,14 @@ public class SoldierMovement : MonoBehaviour
 {
     private SoldierModel m_model;
     private SoldierController m_controller;
+    private SoldierSpriter m_spriter;
     private Coroutine moveCoroutine;
 
-    private void Awake()
+    private void OnEnable()
     {
         m_model = GetComponent<SoldierModel>();
         m_controller = GetComponent<SoldierController>();
-
+        m_spriter = GetComponentInChildren<SoldierSpriter>();
     }
 
     public void MoveTo(Vector2 targetPos)
@@ -51,9 +52,21 @@ public class SoldierMovement : MonoBehaviour
 
         while (Vector2.Distance(transform.position, targetPos) > 0.05f && !m_model.IsDead && !m_model.IsFreezing)
         {
-            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-            yield return null;
-        }
+            Vector2 currentPos = transform.position;
+            Vector2 nextPos = Vector2.MoveTowards(currentPos, targetPos, speed * Time.deltaTime);
+
+            Vector2 delta = nextPos - currentPos;
+
+            // 判断移动方向来设置朝向
+            if (delta.x > 0.01f)
+            {
+                m_spriter.Filp(false);
+            }
+
+            transform.position = nextPos;
+
+                yield return null;
+            }
 
         transform.position = targetPos;
         m_controller.ConvertState(SoldierStateType.IsStaying, true);
