7ff6e0360d419bea61f386524731c3adfc497d56
mcaswen <mcaswen@gmail.com>
2025-07-13 01:12:28 +0800

    feat: implemented a unified animation system interface with callbacks

---
+++ Assets/Game/ImmortalRecord/GameScripts/Soldier/Skills/Animation/SkillEventRelay.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SkillEventRelay : MonoBehaviour
+{
+    private IAnimationEventReceiver receiver;
+
+    public void Init(IAnimationEventReceiver skill)
+    {
+        receiver = skill;
+    }
+
+    public void TriggerEvent(string eventName)
+    {
+
+        CustomLogger.LogWarning("Shoot");
+
+        if (receiver == null) 
+            CustomLogger.LogWarning("Can't find receiver");
+
+        receiver.OnAnimationEvent(eventName);
+    }
+}
