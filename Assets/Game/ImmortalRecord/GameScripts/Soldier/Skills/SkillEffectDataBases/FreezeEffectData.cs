fd51bc8ff4f29da80b8cac3c9ff3cc01639cfe83
mcaswen <mcaswen@gmail.com>
2025-07-13 22:46:57 +0800

    update: removed Freeze skill VFX

--- Assets/Game/ImmortalRecord/GameScripts/Soldier/Skills/SkillEffectDataBases/FreezeEffectData.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Soldier System/SkillEffects/FreezeEffect", order = 18)]
-public class FreezeEffectData : SkillEffectDataBase
-{
-    [Header("冰冻效果设置")]
-    public float freezeDuration;
-}
