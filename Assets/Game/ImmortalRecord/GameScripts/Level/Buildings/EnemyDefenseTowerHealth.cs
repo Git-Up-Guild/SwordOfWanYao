b1787f1732029240d07ab3c1ef93e906411a919c
mcaswen <mcaswen@gmail.com>
2025-07-15 14:13:11 +0800

    feat: implemented the health and damage detection logic for buildings

---
+++ Assets/Game/ImmortalRecord/GameScripts/Level/Buildings/EnemyDefenseTowerHealth.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class EnemyDefenseTowerHealth : MonoBehaviour, IDestructible
+{
+    [SerializeField] public int maxHealth = 1000;
+    [SerializeField] private SoldierCamp camp;
+    [SerializeField] private GameObject AllyDefenseTower;
+    private int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int damage, SoldierModel attacker)
+    {
+
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Instantiate(AllyDefenseTower, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    public SoldierCamp GetCamp() => camp;
+    public int GetHealth() => currentHealth;
+    public int GetMaxHealth() => maxHealth;
+
+    
+}
